986c7b8ebfea5d3fad0365d9d10c1bb2e335137b
Documenter.jl <documenter@juliadocs.github.io>
2024-12-11 21:04:42 +0000

    build based on a5f2022

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlantShapes","category":"page"},{"location":"#PlantShapes","page":"Home","title":"PlantShapes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlantShapes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlantShapes]","category":"page"}]
+}
